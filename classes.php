0f7d15a136cd17a52e9ca38b91aaa9bd39b17ca9
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-26 20:37:14 +0530

    Update classes.php

--- classes.php
+++ classes.php
@@ -4,22 +4,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 echo '<pre/>';
-
-
-// class person {
-// 	public $name;
-// 	public $email;
-// 	private $photo;
-	
-// 	function __construct() {
-//       	print "In constructor\n";		
-// $this->name = "Some name";
-// }
-// }
-// $person1 = new person();
-// print_r($person1->name);
 
 
 class person {
@@ -41,11 +26,12 @@ $person1 = new person('name1', 'email1@example.com');
 print_r($person1->name);
 $person2 = new person('name2', 'email2@example.com', 'photopath');
 print_r($person2->email);
-
 echo PHP_EOL;
+
 echo gettype($person2);
-
 echo PHP_EOL;
-echo get_class($acasttoObject);
+
+// the below is disabled on phptester.net
+// echo get_class($acasttoObject);
 
 
