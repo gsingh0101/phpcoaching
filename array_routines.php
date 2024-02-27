d012b9c5ffaa02638ffaa4d8f74dbaadeffd92c1
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:30:05 +0530

    Update array_routines.php

--- array_routines.php
+++ array_routines.php
@@ -2,7 +2,15 @@
 
 echo '<pre/>';
 
-// array_unique
+// is_array()
+// check if variable is array before using index
+// e.g.
+$a = [1];
+if (is_array($a)) {
+  echo 'it is array <br/>';
+}
+
+// array_unique()
 $repeats = [1,1,1,1,1,3,3,3,3,2,2,2,2];
 print_r(array_unique($repeats));
 // print_r($repeats);
@@ -12,15 +20,16 @@ sort($repeats);
 print_r($repeats);
 
 
+// array_column()
 // extract columns from objects array
-
+// add first object to array
 $o[] = new stdclass();
-$o[0]->a = 1;
-$o[0]->b = 'string1';
-
+$o[0]->id = 1;
+$o[0]->address = 'Lucknow';
+// add second object to array
 $o[] = new stdclass();
-$o[1]->a = 2;
-$o[1]->b = 'string2';
-
-print_r(array_column($o, 'b'));
+$o[1]->id = 2;
+$o[1]->address = 'New Delhi';
+// extract a particular column 'id'
+print_r(array_column($o, 'id'));
 
