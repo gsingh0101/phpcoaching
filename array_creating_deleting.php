beb0e66b6f2852b8d928c5a0f25affbfe7b36a3f
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:21:13 +0530

    Update array_creating_deleting.php

--- array_creating_deleting.php
+++ array_creating_deleting.php
@@ -1,6 +1,7 @@
 <?php
 
 echo '<pre/>';
+echo 'CREATING ARRAYS ....';
 
 // creating array
 $a = array('value1', 'value2', 2);
@@ -10,9 +11,17 @@ $a = ['value1', 'value2', 2];
 $a = array('name' => 'Ravi', 'email' => 'ravi@example.com', 'id' => 'CSE01');
 print_r($a);
 
+echo 'REMOVING ARRAY ELEMENTS ....';
 
 $a = array(0,1,2,3,4,5,6,7);
 // remove 2nd element, with value 1
 unset($a[1]);
 print_r($a);
 
+
+// removing using a logic - like only odd numbers
+$numbers = [1,2,3,4,5,6,7];
+$evens = array_filter($numbers, function ($value) {
+    return ($value % 2 == 0); // return true only for even numbers
+});
+print_r($evens);
