b17112d7999eca4909769cd1460394b8338f2d1b
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 01:01:34 +0530

    Update typehinting.php

--- typehinting.php
+++ typehinting.php
@@ -1,8 +1,8 @@
 <?php
 
-//error_reporting(E_WARNING);
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+echo '<pre/>';
+echo 'Example of type hinting <br/>';
+
 
 function echowitheol($arg)
 {
