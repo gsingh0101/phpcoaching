8b711e132873a3adfb7548400b5840d12827b494
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:32:21 +0530

    Update array_routines.php

--- array_routines.php
+++ array_routines.php
@@ -2,7 +2,7 @@
 
 echo '<pre/>';
 
-// is_array()
+// 1. is_array()
 // check if variable is array before using index
 // e.g.
 $a = [1];
@@ -10,17 +10,19 @@ if (is_array($a)) {
   echo 'it is array <br/>';
 }
 
-// array_unique()
+
+// 2. array_unique()
 $repeats = [1,1,1,1,1,3,3,3,3,2,2,2,2];
 print_r(array_unique($repeats));
 // print_r($repeats);
 
-// sorting
+
+// 3. sorting
 sort($repeats);
 print_r($repeats);
 
 
-// array_column()
+// 4. array_column()
 // extract columns from objects array
 // add first object to array
 $o[] = new stdclass();
