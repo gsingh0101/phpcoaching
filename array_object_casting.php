c367b0a0f8c2fb6c50d11abdbafd47c023378053
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:16:58 +0530

    Update array_object_casting.php

--- array_object_casting.php
+++ array_object_casting.php
@@ -1,1 +1,11 @@
+<?php
+
+// create array from object by casting
+$obj = new stdclass();
+$obj->field1 = 'value1';
+$obj->field2 = 'value2';
+$obj->field3 = 'value3';
+
+$arr = (array)$obj;
+print_r($arr);
 
