37cc4b3afd6d32d80004587c19b1865a76b1d6da
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:18:42 +0530

    Update array_object_casting.php

--- array_object_casting.php
+++ array_object_casting.php
@@ -9,3 +9,8 @@ $obj->field3 = 'value3';
 $arr = (array)$obj;
 print_r($arr);
 
+// create object from array
+$a = array('name' => 'Ravi', 'email' => 'ravi@example.com', 'id' => 'CSE01');
+$object_from_array = (object)$a;
+print_r($object_from_array);
+
