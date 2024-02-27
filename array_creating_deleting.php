7b7a2baffccf111bf259e7ac1a9f8c6897cbe692
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 00:14:06 +0530

    Update array_creating_deleting.php

--- array_creating_deleting.php
+++ array_creating_deleting.php
@@ -7,11 +7,11 @@ $a = array('value1', 'value2', 2);
 $a = ['value1', 'value2', 2];
 
 // creating associative array
-$a = array('name' => 'Ravi', 'email' => 'ravi@example.com', id => 'CSE01');
+$a = array('name' => 'Ravi', 'email' => 'ravi@example.com', 'id' => 'CSE01');
 print_r($a);
 
 
 $a = array(0,1,2,3,4,5,6,7);
-// remove 2nd element
+// remove 2nd element, with value 1
 unset($a[1]);
 print_r($a);
