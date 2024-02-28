ac7e22415e67b710a8f4d38c03764c0119ebc038
gsingh0101 <gsingh0101@users.noreply.github.com>
2024-02-28 12:22:05 +0530

    Update typehinting.php

--- php-concepts/typehinting.php
+++ php-concepts/typehinting.php
@@ -12,10 +12,13 @@ function echowitheol($arg)
 function jointhings(int $a, float $b): string {
     return $a . $b;
 }
-$a = jointhings("a1", 1.1);
+// $a = jointhings("a1", 1.1); // does not work as first parameter is specified as int but string passed
+$a = jointhings("1", 1.1); // works because PHP internally interprets strings which are convertible to numerals as numerals
 echowitheol($a);
-$a = jointhings("1.1", 1.1);
-echowitheol($a);
+
+// $a = jointhings("1.1", 1.1); // does not work because 1.1 CANNOT be interpretted integer but ONLY as a float
+// echowitheol($a);
+
 
 function jointhings_using_uniontypes(string|int $a, float $b): string {
     return $a . $b;
